3be74b475b572d3011f467ab610711096430244c
nanis28 <66491509+nanis28@users.noreply.github.com>
2020-07-18 11:24:59 -0300

    Upadate - R Programming Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## R Programming - Programming Assignment 2
 
-makeCacheMatrix <- function(x = matrix()) {
+## This function creates a special "matrix" object that can cache its inverse.
+# Mudanças: atribuir o valor "s" como NULL e troca a função "mean" para "solve"
 
+makeCacheMatrix <- function(x = matrix(sample(1:20,6), 3,3)) {
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) s <<- solve
+        getsolve <- function() s
+        list(set = set, get = get,
+             setsolve = setsolve,
+             setsolve = setsolve)
 }
 
+makeCacheMatrix()
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+#If the inverse has already been calculated (and the matrix has not changed), then cacheSolve should 
+#retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()
+        if(!is.null(s)) {
+                message ("getting inversed matrix")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
 }
+
+
